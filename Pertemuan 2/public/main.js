8c5d536e3f489a5aceaa286675dd51d2a6358e0e
Muhammad Nafrizal <dzaldut9@gmail.com>
2024-02-28 19:49:11 +0700

    Pertemuan 2

--- Pertemuan 2/public/main.js
+++ Pertemuan 2/public/main.js
@@ -66,7 +66,7 @@ const vertexShaderSource = `
 const fragmentShaderSource = `
 precision mediump float;
   void main() {
-      gl_FragColor = vec4(1, 0, 0, 1); // Warna titik
+      gl_FragColor = vec4(0, 0, 0, 1); // Warna titik
   }
 `;
 
